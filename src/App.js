f91661cb624297cdb83336df4a263c6c081adf2c
Stephen Pierzchala <pierzcha@usc.edu>
2022-10-08 13:24:22 -0500

    Initial Commit, add tailwind and react

---
+++ src/App.js
@@ -0,0 +1,11 @@
+export function App() {
+	return (
+		<>
+			<button className="p-2 rounded bg-blue-500 hover:bg-blue-600 transition">
+				Test
+			</button>
+			<h1 class="text-3xl font-bold underline">Hello world!</h1>
+			<div className="bg-blue-500"></div>
+		</>
+	);
+}
